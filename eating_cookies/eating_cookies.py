e58bf37df7cb18e85c60d36c5a5022866e9b3540
robomantis19 <freefavors@gmail.com>
2020-05-20 18:54:10 -0400

    attempted all 5 problems and one stretch

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,10 +2,28 @@
 Input: an integer
 Returns: an integer
 '''
+
+# 3 cookies
+
+# 1 1 1
+# 2 1
+# 3
+num = None
 def eating_cookies(n):
     # Your code here
-
-    pass
+    if n == 0 or n == 1 : 
+        return 1
+    total = 0
+    one = n/1
+    two = n/2
+    three = n/3
+    if n > 1: 
+        stepTotal = one + two + three
+        num = eating_cookies(stepTotal)     
+        total += num
+    
+    else: 
+        return total
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
