1a266e7325525b9492b8261f65cdea66c0b3d44b
robomantis19 <freefavors@gmail.com>
2020-05-21 18:16:17 -0400

    made some progress

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -8,22 +8,34 @@ Returns: an integer
 # 1 1 1
 # 2 1
 # 3
-num = None
+
 def eating_cookies(n):
     # Your code here
-    if n == 0 or n == 1 : 
+
+    if n == 0: 
         return 1
-    total = 0
-    one = n/1
-    two = n/2
-    three = n/3
-    if n > 1: 
-        stepTotal = one + two + three
-        num = eating_cookies(stepTotal)     
-        total += num
+    elif n < 0: 
+        return 0
+
+    return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+
+
+
+
+
+    # if n == 0 or n == 1 : 
+    #     return 1
+    # total = 0
+    # one = n/1
+    # two = n/2
+    # three = n/3
+    # if n > 1: 
+    #     stepTotal = one + two + three
+    #     num = eating_cookies(stepTotal)     
+    #     total += num
     
-    else: 
-        return total
+    # else: 
+    #     return total
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -7,24 +7,39 @@ Returns: a List of integers
 
 def moving_zeroes(arr):
     # Your code here
+    left = 0 
+    right = len(arr) - 1
 
-    for i in range(0, len(arr)): 
-        if arr[i] == 0: 
-            zero = i
+    while left <= right: 
+        if arr[left] == 0 and arr[right] != 0: 
+            arr[left], arr[right] = arr[right], arr[left]
+            left += 1
+            right -= 1
         else: 
-            nonzero = i
-            if nonzero != None: 
-                while zero < nonzero:
-                    arr[zero], arr[nonzero] = arr[nonzero], arr[zero]
-        if arr[i+1] != 0: 
-            nonzero2 = i+1
-            while zero2 < nonzero2: 
-                arr[zero2], arr[nonzero2] = arr[nonzero2], arr[zero2]
-        else: 
-            zero2 = i+1
-        
-        
+            if arr[left] != 0: 
+                left += 1
+            if arr[right] == 0: 
+                arr[right] -= 1
     return arr
+
+    # for i in range(0, len(arr) -1 ): 
+    #     if arr[i] == 0: 
+    #         zero = i
+    #     else: 
+    #         nonzero = i
+    #         if nonzero != None: 
+    #             while arr[zero] < arr[nonzero]:
+    #                 arr[zero], arr[nonzero] = arr[nonzero], arr[zero]
+
+    #     if arr[i+1] != 0: 
+    #         nonzero2 = i+1
+    #         while arr[zero2] < arr[nonzero2]: 
+    #             arr[zero2], arr[nonzero2] = arr[nonzero2], arr[zero2]
+    #     else: 
+    #         zero2 = i+1
+        
+        
+    # return arr
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
